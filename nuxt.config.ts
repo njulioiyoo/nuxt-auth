a94e5f07473baa51cf15fff7450e0bf27763d2e2
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2024-12-13 21:34:53 +0700

    feat(app): ensure login page is displayed on app initialization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui"],
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  router: {
+    middleware: ["auth"],
+  },
 });
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { navigateTo } from 'nuxt/app';
+
+// Redirect ke login saat halaman root diakses
+navigateTo('/login');
+</script>
+
+<template>
+    <div></div>
+</template>
